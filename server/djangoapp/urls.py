760e4444b261c0e00a9a8daafad8def7b44f4de2
M. Adilla Ramadhan <muh.adillaramadhan@gmail.com>
2024-11-24 15:49:37 +0700

    Change: modul 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
 
     path(route='register', view=views.registration_user, name='register'),
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for dealer reviews view
 
     # path for add a review view
